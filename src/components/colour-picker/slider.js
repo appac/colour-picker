4a77482a63d2af52a0e0e2bbb27a6e9298ecba62
Chris Apparicio <chris@apparic.io>
2021-03-10 17:49:18 +0000

    feat(slider): add and style hue slider, sends ImageData back up, name export from slider

--- src/components/colour-picker/slider.js
+++ src/components/colour-picker/slider.js
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./slider.scss";
 
-export default ({ handleHueChange }) => {
-  return <div>Slider</div>;
+const Slider = ({ handleHueChange }) => {
+  const hueCanvas = useRef(null);
+
+  useEffect(() => {
+    drawHueSlider();
+  });
+
+  const drawHueSlider = () => {
+    const context = hueCanvas.current.getContext("2d");
+    const gradientFill = context.createLinearGradient(0, 0, 0, 200);
+    gradientFill.addColorStop(0, "rgb(255, 0, 0)");
+    gradientFill.addColorStop(0.16, "rgb(255, 255, 0)");
+    gradientFill.addColorStop(0.32, "rgb(0, 255, 0)");
+    gradientFill.addColorStop(0.46, "rgb(0, 255, 255)");
+    gradientFill.addColorStop(0.64, "rgb(0, 0, 255)");
+    gradientFill.addColorStop(0.8, "rgb(255, 0, 255)");
+    gradientFill.addColorStop(1.0, "rgb(255, 0, 0)");
+    context.fillStyle = gradientFill;
+    context.fillRect(0, 0, 30, 200);
+  };
+
+  const redrawHueSlider = () => {
+    const context = hueCanvas.current.getContext("2d");
+    context.clearRect(0, 0, 30, 200);
+    drawHueSlider();
+  };
+
+  const getHueValue = (pickLocation) => {
+    const context = hueCanvas.current.getContext("2d");
+    handleHueChange(context.getImageData(15, pickLocation, 1, 1));
+  };
+
+  // FIXME: Picker circle isn't being drawn. Layer/order issue with redraws?
+  const drawPickerCircle = (pickLocation) => {
+    redrawHueSlider();
+    const context = hueCanvas.current.getContext("2d");
+    context.beginPath();
+    context.arc(15, pickLocation, 5, 0, Math.PI * 2);
+    context.stroke();
+    context.closePath();
+  };
+
+  return (
+    <div className="hue-slider">
+      <canvas
+        id="slider"
+        className="hue-slider__canvas"
+        ref={hueCanvas}
+        width="30"
+        height="200"
+      />
+      <input
+        className="hue-slider__control"
+        type="range"
+        orient="vertical"
+        min="0"
+        max="200"
+        step="1"
+        defaultValue="100"
+        onChange={(e) => {
+          const pickLocation = (200 * e.target.value) / 100;
+          getHueValue(pickLocation);
+          drawPickerCircle(pickLocation);
+        }}
+      />
+    </div>
+  );
 };
+
+export default Slider;
